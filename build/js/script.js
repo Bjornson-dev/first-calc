925e974bba7467ac707f400b67884f520fb50f25
Bjornson <arthurbearnyson@mail.ru>
2020-08-17 16:19:15 +0600

    Добавил RippleAnimation

--- build/js/script.js
+++ build/js/script.js
@@ -1,43 +1,10 @@
 "use strict";
 
-// ////// Анимация цифр
-// let user = {},
-//     glowBtn = document.querySelector('.glowAnim');
-
-// document.onmousemove = getGlow;
-// // найти центр в кнопке
-// let btnPos = glowBtn.getBoundingClientRect(),
-//     btnCenterX = btnPos.x + (btnPos.width / 2),
-//     btnCenterY = btnPos.y + (btnPos.height / 2);
-
-// // Анимация при приближении
-// function getGlow(event) {
-//   let x = 0, y = 0, 
-//       btnUserDistanceX = user.x - btnCenterX,
-//       btnUserDistanceY = user.y - btnCenterY;
-//   // Определяем место курсора
-//   x = window.event.clientX;
-//   y = window.event.clientY;
-
-//   user.x = x;
-//   user.y = y;
-
-//   console.log(btnUserDistanceX + " || " + btnUserDistanceY);
-//   // Манипулируем свечением
-//   if ( (Math.abs(btnUserDistanceX) < 60 )  && ( Math.abs(btnUserDistanceY)) < 60 ) {
-//     glowBtn.style.boxShadow = `0 0 ${30 - ( Math.max(Math.abs(btnUserDistanceX) + Math.abs(btnUserDistanceY) ) / 3)}px rgb(0, 225, 255)`
-//     console.log("y")
-//   } else {
-//     glowBtn.style.boxShadow = `0 0 0px rgb(0, 225, 255)`
-//   }
-// }  
-
 /////// Анимация кнопок ввода
 let cursor = {},
     glowButtons = document.querySelectorAll(".glowAnim");
 
 document.onmousemove = () => btnGlowAnimation(event, glowButtons);
-
 
 function btnGlowAnimation(event, buttons) {
   let userX = window.event.clientX, // положение курсора
@@ -69,6 +36,17 @@ function addGlowAnimation(btn, btnX, btnY, userX, userY) {
 }
 
 
+////// Анимация кликов
+glowButtons.forEach( (elem) => {
+  elem.onclick = () => btnClickAnimation(elem);
+});
 
 
+function btnClickAnimation(btn) {
+  let rippleX = window.event.clientX - btn.getBoundingClientRect().x,
+      rippleY = window.event.clientY - btn.getBoundingClientRect().y;
 
+
+  btn.insertAdjacentHTML('beforeend', `<div class="ripple" style="top:${rippleY - 50}px; left:${rippleX - 50}px"></div>`);
+  setTimeout(() => btn.querySelector("div.ripple").remove(), 500);
+}
